57c46cc3cd697f6910b5f9beba8e869493578789
Durgesh Rao <77789927+DURGESH716@users.noreply.github.com>
2022-08-13 23:51:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ model = pickle.load(open("flight_rf.pkl", "rb"))
 @app.route("/")
 @cross_origin()
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 
@@ -356,10 +356,10 @@ def predict():
 
         output=round(prediction[0],2)
 
-        return render_template('home.html',prediction_text="Your Flight price is Rs. {} only".format(output))
+        return render_template('index.html',prediction_text="Your Flight price is Rs. {} only".format(output))
 
 
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 
